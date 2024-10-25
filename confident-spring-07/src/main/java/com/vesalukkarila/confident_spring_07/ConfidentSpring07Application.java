f1cef26be2d54edc6f33103f01c5cac852a3be22
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-25 17:21:28 +0300

    copy files from previous chapter, hook up to use self made autoconfiguration

--- confident-spring-07/src/main/java/com/vesalukkarila/confident_spring_07/ConfidentSpring07Application.java
+++ confident-spring-07/src/main/java/com/vesalukkarila/confident_spring_07/ConfidentSpring07Application.java
@@ -1,11 +1,22 @@
 package com.vesalukkarila.confident_spring_07;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ConfidentSpring07Application {
 
+
+	@Bean
+	public InitializingBean runner(DataSource dataSource) {
+		return () -> {
+			System.out.println("This is the datasource this Spring Boot project is using: " + dataSource);
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConfidentSpring07Application.class, args);
 	}
