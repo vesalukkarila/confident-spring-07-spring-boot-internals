24a6518f6ca08a8c4b9c494f4fb2b6a4b334d6d5
Vesa Lukkarila <github@vesalukkarila.com>
2024-10-28 17:38:03 +0200

    fix broken code

--- confident-spring-07/src/main/java/com/vesalukkarila/confident_spring_07/dto/InvoiceDto.java
+++ confident-spring-07/src/main/java/com/vesalukkarila/confident_spring_07/dto/InvoiceDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
+
 public class InvoiceDto {
 
     @JsonProperty("user_id")
